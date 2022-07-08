7167df01b0ebe4c0801c2592c59029f45ab325c0
harrisonvshen <96361172+harrisonvshen@users.noreply.github.com>
2022-07-07 23:43:29 -0700

    Update bump-solidity.js

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -10,3 +10,14 @@ bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+let content = fs.readFileSync("./contracts/Multiplier3Verifier.sol", { encoding: 'utf-8' });
+let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
+bumped = bumped.replace(verifierRegex, 'contract Multiplier3Verifier');
+
+fs.writeFileSync("./contracts/Multiplier3Verifier.sol", bumped);
+
+let content = fs.readFileSync("./contracts/Multiplier3_plonkVerifier.sol", { encoding: 'utf-8' });
+let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
+bumped = bumped.replace(verifierRegex, 'contract Multiplier3_plonkVerifier');
+
+fs.writeFileSync("./contracts/Multiplier3_plonkVerifier.sol", bumped);
